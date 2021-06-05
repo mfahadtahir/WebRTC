d267c253e4b27df150c7d2bfff8c1326aef18703
mfahadtahir <muhammadfahad325302@gmail.com>
2021-06-05 18:23:03 +0500

    heroku bug

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.get("/", (req, res) => {
   res.render("main", { foo: "FOO" });
 });
 
-app.listen(4000, () => console.log("Example app listening on port 4000!"));
+app.listen(process.env.PORT || 4000, () => console.log("Example app listening on port 4000!"));
